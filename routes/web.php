9d0fd054167d007636fb01c4dbdeb3d944bb894f
Kazuki Baba <kzhrs4002@gmail.com>
2024-09-27 16:07:08 +0900

    auth completed

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
+|--------------------------------------------------------------------------|
+| Web Routes                                                               |
+|--------------------------------------------------------------------------|
+| Here is where you can register web routes for your application. These    |
+| routes are loaded by the RouteServiceProvider within a group which       |
+| contains the "web" middleware group. Now create something great!         |
+|--------------------------------------------------------------------------|
 */
 
+// デフォルトルート: ログイン画面にリダイレクト
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
+});
+
+// ログインページのルート
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+// ログイン処理
+Route::post('/login', [AuthController::class, 'login']);
+
+// ログアウト処理
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// 認証が必要なルート
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    // ダッシュボード（ホーム画面）
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
 });
