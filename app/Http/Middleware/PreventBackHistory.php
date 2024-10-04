11221bf9b66899b002206bb7287896e8a17bee1b
Kazuki Baba <kzhrs4002@gmail.com>
2024-10-04 14:12:34 +0900

    no cache ok

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -17,6 +17,7 @@ class PreventBackHistory
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
+    
 
         return $response->header('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0')
                         ->header('Pragma', 'no-cache')
