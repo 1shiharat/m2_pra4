60e8bc905d8524b5bbf39bb1abc9df2b105ef037
Kazuki Baba <kzhrs4002@gmail.com>
2024-10-04 13:43:26 +0900

    auth update2

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -4,21 +4,20 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class PreventBackHistory
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        $response = $next($request);
 
-
-        // キャッシュを無効化するためのヘッダーを追加
         return $response->header('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0')
                         ->header('Pragma', 'no-cache')
                         ->header('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
